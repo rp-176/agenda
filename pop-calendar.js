59086871175c98b11423ed8241b8fb4c4078aee9
rp-176 <prishi2020@gmail.com>
2022-08-27 15:07:18 -0400

    update

--- pop-calendar.js
+++ pop-calendar.js
@@ -1,17 +1,37 @@
-import calendarize from 'C:/Users/patel/AppData/Roaming/npm/node_modules/calendarize';
-
 let currentDate = new Date();
 const calendarElement = document.getElementById("calendar");
 
 
+function calendarize(target, offset) {
+	var i=0, j=0, week, out=[], date = new Date(target || new Date);
+	var year = date.getFullYear(), month = date.getMonth();
+
+	//day index (of week) for 1st of month
+	var first = new Date(year, month, 1 - (offset | 0)).getDay();
+
+	// how many days there are in this month
+	var days = new Date(year, month+1, 0).getDate();
+
+	while (i < days) {
+		for (j=0, week=Array(7); j < 7;) {
+			while (j < first) week[j++] = 0;
+			week[j++] = ++i > days ? 0 : i;
+			first = 0;
+		}
+		out.push(week);
+	}
+	return out;
+}
+
+
 function populateElement(date){
-    let getMonth = calendarize(date);
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
+    let month = calendarize(date);
+    for (let i = 0; i < month.length; i++) {
+        for (let j = 0; j < 7; j++) {
             let dayElement = document.createElement("div");
-            dayElement.innerText = getMonth[i][j];
+            dayElement.innerText = month[i][j];
             dayElement.classList.add("day");
-            if(getMonth[i][j] == 0){
+            if(month[i][j] == 0){
                 dayElement.classList.add("dummy-day");
             }
             if(j == 0 | j ==6){
@@ -22,4 +42,6 @@ function populateElement(date){
     }
 }
 
-populateElement(currentDate);
+populateElement("Aug 16 2022");
+console.log("HELLO");
+
