05f13f0fe22d6a4aba21208101f3f97decd85f53
rp-176 <prishi2020@gmail.com>
2022-08-28 13:18:41 -0400

    update

--- pop-calendar.js
+++ pop-calendar.js
@@ -1,14 +1,21 @@
-let currentMonth, currentYear;
+var  currentDate, currentMonth, currentYear;
 var listOfMonths= ["January","February","March","April","May","June","July",
             "August","September","October","November","December"];
+var listOfDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 
-let currentDate = new Date();
+currentDate = new Date();
 currentMonth = currentDate.getMonth();
 currentYear = currentDate.getFullYear();
+          
+
 const calendarElement = document.getElementById("calendar");
 const monthTitle = document.getElementById("month-title");
 
+
+let setMonthTitle = (month) => document.getElementById("mtext-cont").innerText = listOfMonths[month].toUpperCase();
+
+//calendarize library import kept throwing error
 function calendarize(target, offset) {
 	var i=0, j=0, week, out=[], date = new Date(target || new Date);
 	var year = date.getFullYear(), month = date.getMonth();
@@ -33,15 +40,21 @@ function calendarize(target, offset) {
 
 function populateElement(date){
     let month = calendarize(date);
-    for (let i = 0; i < month.length; i++) {
+    for (let i = 0; i <= 6; i++) { //add days of the week to calendar id
+        let dow = document.createElement("div");
+        dow.innerText = listOfDays[i];
+        dow.classList.add("dow");   //add class of dow
+        calendarElement.appendChild(dow); //append element to calendar id
+    }
+    for (let i = 0; i < month.length; i++) { //calendarize returns a 2d array of (day of week) x (number of weeks)
         for (let j = 0; j < 7; j++) {
             let dayElement = document.createElement("div");
-            dayElement.innerText = month[i][j];
-            dayElement.classList.add("day");
-            if(month[i][j] == 0){
-                dayElement.className = '';
+            dayElement.innerText = month[i][j]; //adds date to created div
+            dayElement.classList.add("day"); 
+            if(month[i][j] == 0){ //if date is 0 meaning it is from previous month
+                dayElement.className = ''; //empty class list and it's inner text
                 dayElement.classList.add("dummy-day");
-                dayElement.innerText = null;
+                dayElement.innerText = null; 
             }
             if(j == 0 | j ==6){
                 dayElement.classList.add("weekend");
@@ -51,9 +64,43 @@ function populateElement(date){
     }
 }
 
-let setMonthTitle = (month) => document.getElementById("mtext-cont").innerText = listOfMonths[month].toUpperCase;
+let nextMonth = function(id){
+    let button = document.getElementById(id);
+    button.addEventListener("click", () => { //add eventlistener to passed button
+        calendarElement.innerHTML = ""; //empty calendar id before displaying new month    
+        if(id == "backbtn"){
+            if(currentMonth == 0){ //switch to previous year if current month is jan
+                currentMonth = 11;
+                currentYear -= 1;
+            }
+            else{
+                currentMonth -= 1;
+            }
+            
+            currentDate.setMonth(currentMonth);
+            currentDate.setFullYear(currentYear);            
+            setMonthTitle(currentMonth);
+            populateElement(currentDate);
+        }
+        if(id == "nextbtn"){
+            if(currentMonth == 11){
+                currentMonth = 0;
+                currentYear += 1;
+            }
+            else{
+                currentMonth += 1;
+            }
 
- 
+            currentDate.setMonth(currentMonth);
+            currentDate.setFullYear(currentYear);             
+            setMonthTitle(currentMonth);
+            populateElement(currentDate);
+        }
+    });
+}
+
+//init
 populateElement(currentDate);
-// setMonthTitle(currentMonth);
-
+setMonthTitle(currentMonth);
+nextMonth("backbtn");
+nextMonth("nextbtn");
